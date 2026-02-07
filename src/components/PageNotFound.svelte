9dff521f06430f7593ff6543d09b050197410803
Anton G Neuhold Jr <agneuhold@gmail.com>
2026-02-07 13:45:05 -0800

    Remove unused components

--- src/components/PageNotFound.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-  import PageTitle from './PageTitle.svelte';
-</script>
-
-<PageTitle includeBreadcrumb={false} title="Page Not Found 🥺" />
--- src/components/singletons/Confetti/SBConfettiDecorator.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import type { Snippet } from 'svelte';
-  import Confetti from './Confetti.svelte';
-
-  let { children }: { children: Snippet } = $props();
-</script>
-
-<Confetti />
-{@render children()}
