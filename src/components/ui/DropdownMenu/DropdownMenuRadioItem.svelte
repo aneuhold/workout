1b47a4fb3d071621953d4679062df827723edcf2
Anton G Neuhold Jr <agneuhold@gmail.com>
2026-02-08 17:06:16 -0800

    Initially add a ton of components

---
+++ src/components/ui/DropdownMenu/DropdownMenuRadioItem.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { IconCircleFilled } from '@tabler/icons-svelte';
+  import { DropdownMenu as DropdownMenuPrimitive } from 'bits-ui';
+  import { cn, type WithoutChild } from '$util/svelte-shadcn-util.js';
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    children: childrenProp,
+    ...restProps
+  }: WithoutChild<DropdownMenuPrimitive.RadioItemProps> = $props();
+</script>
+
+<DropdownMenuPrimitive.RadioItem
+  bind:ref
+  data-slot="dropdown-menu-radio-item"
+  class={cn(
+    "focus:bg-accent focus:text-accent-foreground relative flex cursor-default items-center gap-2 rounded-sm py-1.5 ps-8 pe-2 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+    className
+  )}
+  {...restProps}
+>
+  {#snippet children({ checked })}
+    <span class="pointer-events-none absolute start-2 flex size-3.5 items-center justify-center">
+      {#if checked}
+        <IconCircleFilled class="size-2 fill-current" />
+      {/if}
+    </span>
+    {@render childrenProp?.({ checked })}
+  {/snippet}
+</DropdownMenuPrimitive.RadioItem>
