f7d12b6aaf5fd456441d06e4e604fae091a0f101
Anton G Neuhold Jr <agneuhold@gmail.com>
2026-02-18 17:18:38 -0800

    Add confetti component

---
+++ src/components/singletons/Confetti/Confetti.stories.svelte
@@ -0,0 +1,16 @@
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import { createNumberArgTypes } from '$storybook/storybookUtil';
+  import SbConfettiExample from './SBConfettiExample.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Singleton Components/Confetti',
+    component: SbConfettiExample,
+    argTypes: {
+      ...createNumberArgTypes('numButtons')
+    }
+  });
+</script>
+
+<Story name="Default" args={{ numButtons: 1 }} />
+<Story name="Multiple Confetti" args={{ numButtons: 55 }} />
