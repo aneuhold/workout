54586607f8013474f50a429806a26d0cd9e9ec77
Anton G Neuhold Jr <agneuhold@gmail.com>
2026-04-21 18:04:05 -0700

    Fix types

--- src/app.d.ts
+++ src/app.d.ts
@@ -21,6 +21,13 @@ declare global {
   interface Document {
     startViewTransition?(callback: () => Promise<void> | void): ViewTransition;
   }
+
+  // This has to be done because as of 2/16/2026 it seems that there is a bug in the Svelte
+  // TypeScript where it says that $state.snapshot returns a map of Snapshot<T> instead of T,
+  // even though the docs say it should return T. This is a workaround for now.
+  namespace $state {
+    function snapshot<T>(state: T): T;
+  }
 }
 
 export {};
