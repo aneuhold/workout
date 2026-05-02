b74f6e481ef105fe5f6d78c515d595583bd7793e
Anton G Neuhold Jr <agneuhold@gmail.com>
2026-05-02 13:07:25 -0700

    Update localOverride

--- src/util/localOverride.ts
+++ src/util/localOverride.ts
@@ -1,4 +1,5 @@
 import { APIService } from '@aneuhold/core-ts-api-lib';
+import { Capacitor } from '@capacitor/core';
 import { createLogger } from '$util/logging/logger';
 
 const log = createLogger('localOverride.ts');
@@ -10,7 +11,12 @@ export default function localOverride() {
   const enableLocalOverride = false;
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   if (enableLocalOverride) {
-    log.debug('Local override active: Using local API URL');
-    APIService.setAPIUrl('http://localhost:8080/');
+    const apiUrl = Capacitor.isNativePlatform()
+      ? // What the android emulator uses to access localhost of the host machine
+        'http://10.0.2.2:8080/'
+      : // Normal localhost for web
+        'http://localhost:8080/';
+    log.debug(`Local override active: Using local API URL ${apiUrl}`);
+    APIService.setAPIUrl(apiUrl);
   }
 }
