0861fda060b1d3b93cd3f2c7cd79d58e7df4cf51
Anton G Neuhold Jr <agneuhold@gmail.com>
2026-06-02 18:13:25 -0700

    Bump version and add comment

--- src/components/TopBar/TopBar.svelte
+++ src/components/TopBar/TopBar.svelte
@@ -43,6 +43,8 @@
   }
 </script>
 
+<!-- Take a look at notes about backface-hidden / backface-visibility, but if that is removed, then newer versions of
+ Android have an absolute wrecked UI. -->
 <header
   class="[view-transition-name:match-element] backface-hidden z-40 flex h-(--top-nav-height) items-center bg-sidebar text-sidebar-foreground
     {showTimerHighlight ? 'fixed inset-x-0 top-0' : 'md:fixed md:inset-x-0 md:top-0'}"
